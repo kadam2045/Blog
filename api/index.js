e7d6376ddf14233cb43ab34078cd905c77ed0954
Shubham Kadam <shubham.kadam@appgambit.com>
2024-01-24 14:16:58 +0530

    connection to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log("coonnection issue ", err);
+  });
 const app = express();
 
-app.listen(5000,() =>{
-    console.log("server is running on port 5000")
-})
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
+});
