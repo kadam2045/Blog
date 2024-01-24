f3ab0da1e4a880c1bdfa22ba474bff48c1225068
Shubham Kadam <shubham.kadam@appgambit.com>
2024-01-24 18:52:17 +0530

    add functionality to sign-up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,47 @@
 import { Button } from "@mui/material";
-import { Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import Alert from "@mui/material/Alert";
+
+import { Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      setErrorMessage("Please fill  all fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+      const data = await res.json();
+
+      console.log(data, "data");
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+     
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
   return (
     <div className="min-h-screen mt-20 ">
       <div className="flex p-3 max-w-6xl mx-auto flex-col md:flex-row md:items-center">
@@ -16,32 +55,56 @@ const SignUp = () => {
         </div>
 
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your Email" />
-              <TextInput type="email" placeholder="Email" id="email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Password" />
-              <TextInput type="password" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button variant="contained" type="submit">
-              Sign Up
+            <Button variant="contained" type="submit" disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size="sm" /> <span className="pl-3">Loading...</span>{" "}
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
             <div className="flex gap-2 text-sm mt-6">
-          <span>Have an account?</span>
-          <Link to="/sign-in" className="text-blue-700">
-            Sign In
-          </Link>
-        </div>
+              <span>Have an account?</span>
+              <Link to="/sign-in" className="text-blue-700">
+                Sign In
+              </Link>
+            </div>
+            {errorMessage && (
+              <Alert className="mt-5" ceverity="info">
+                {errorMessage}
+              </Alert>
+            )}
           </form>
         </div>
-
-        
       </div>
     </div>
   );
